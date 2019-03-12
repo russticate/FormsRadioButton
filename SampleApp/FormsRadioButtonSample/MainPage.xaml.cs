91e862cf2103cf1e9da7ca7a84f1ba6f8a0a4541
russticate <45570423+russticate@users.noreply.github.com>
2019-03-12 14:09:35 +0000

    Added event that can be published when the toggled selection changes. Added event to sample project. Cleaned code in sample project and removed unrequired parameter from the RadioItemToggledEventArgs.

--- SampleApp/FormsRadioButtonSample/MainPage.xaml.cs
+++ SampleApp/FormsRadioButtonSample/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Xamarin.Forms;
+﻿using FormsRadioButton;
+using Xamarin.Forms;
 
 namespace FormsRadioButtonSample
 {
@@ -9,6 +10,31 @@ namespace FormsRadioButtonSample
             Xamarin.Forms.PlatformConfiguration.iOSSpecific.Page.SetUseSafeArea(this, true);
             InitializeComponent();
             BindingContext = new MainPageViewModel();
+            NotifyViaEvent.Toggled += NotifyViaEvent_Toggled;
+        }
+
+        void NotifyViaEvent_Toggled(object sender, ToggledEventArgs e)
+        {
+            if (sender is Switch eventSwitch)
+            {
+                if (eventSwitch.IsToggled)
+                {
+                    FormsRadioButton.ItemToggled += FormsRadioButton_ItemToggled;
+                }
+
+                else
+                {
+                    FormsRadioButton.ItemToggled -= FormsRadioButton_ItemToggled;
+                }
+
+                ManualSelectButton.IsEnabled = !eventSwitch.IsToggled;
+            }
+        }
+
+        // You could always use a behavior to forward this event to a command if you wanted to go full MVVM.
+        async void FormsRadioButton_ItemToggled(object sender, RadioItemToggledEventArgs e)
+        {
+            await Application.Current?.MainPage.DisplayAlert("Hi :)", $"Manual selection is item: {e.SelectedItem.Text}", "Cool, thanks");
         }
     }
 }
